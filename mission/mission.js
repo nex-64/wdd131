d6fdba9f572a7bf1b7c7147583823dfc94cb40b9
nex-64 <natepod123@gmail.com>
2026-01-17 17:27:46 -0700

    Prove: mission statement II

--- mission/mission.js
+++ mission/mission.js
@@ -2,21 +2,25 @@
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
 let body = document.querySelector('body');
+let main = document.querySelector('main');
+let h2 = document.querySelector('h2');
 
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        body.style.backgroundColor = 'black';
+        body.style.backgroundColor = '#333';
         body.style.color = 'white';
         logo.setAttribute('src', 'images/byui-logo-dark.png');
-        document.getElementById('content').style.borderColor = 'white'
+        main.style.borderColor = 'white'
+        h2.style.color = '#71b5d3'
     } else {
         body.style.backgroundColor = 'white';
         body.style.color = 'black';
         logo.setAttribute('src', 'images/byui-logo-blue.webp');
-        document.getElementById('content').style.borderColor = 'black'
+        main.style.borderColor = 'black'
+        h2.style.color = '#0165a3'
     }
 }           
                     
